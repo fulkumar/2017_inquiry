0d094a7b4317679ec36e4e7001f2fc6fc35e4f3d
fulkumar <fulkumar1990@gmail.com>
2017-04-25 18:49:23 +0900

    secound commit.

---
+++ wwwroot/inquiry.php
@@ -0,0 +1,15 @@
+<?php
+//inquiry
+?>
+
+<html>
+<body>
+      <form action="./inquiry_fin.php" method="post">
+	Email Adress(*):<input type="text" name="email"><br>
+	Name:<input type="text" name="email"><br>
+	Birthday:<input type="text" name="birthday"><br>
+	Inquiry:<textarea name="body"></textarea><br>
+	<button>Inquiry</button>
+</form>
+</body>
+</html>
