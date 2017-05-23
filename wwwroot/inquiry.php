9eb53ff6cb8719475bb43b0b0834f420a20835cf
fulkumar <fulkumar1990@gmail.com>
2017-05-23 17:27:15 +0900

    DB insert

--- wwwroot/inquiry.php
+++ wwwroot/inquiry.php
@@ -24,6 +24,7 @@ if (true === isset($_SESSION['buffer']['error_detail'])) {
     //$error_detail = []; // PHP 5.4以降ならこっちでもよい
     $error_detail = array();
 }
+var_dump($error_detail);
 
 
 //CSRF トークン作成
